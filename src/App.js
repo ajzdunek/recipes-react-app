13157e2147194217669d314ceb17f1d51a0f6653
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2020-09-02 17:09:06 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,84 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // import logo from './logo.svg';
+import Recipes from './Recipes'
+
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+
+  const APP_ID = 'a02699c1';
+  const APP_KEY = '520002ee3a33209a319d3f8eb5308771';
+  // const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [recipes, setRecipes] = useState([]);
+
+   // ---------------------------------------
+
+  const [search, setSearch] = useState('');
+
+  // ---------------------------------------
+
+  const [query, setQuery] = useState('chicken')
+
+  // ---------------------------------------
+  
+  useEffect(() => {
+
+   getRecipes();
+
+  }, [query])
+
+  // ---------------------------------------
+
+    const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits)
+  } 
+
+ // ---------------------------------------
+
+ const updateSearch = e => {
+   setSearch(e.target.value)
+ }
+
+ const getSearch = e => {
+   e.preventDefault();
+   setQuery(search);
+   setSearch('');
+ }
+
   return (
+    
     <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+    
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+
+          <button className="search-button" type="submit">Search</button>
+    
+      </form>
       
+    {
+      
+      recipes.map(recipe =>(
+
+      <Recipes 
+
+      key={recipe.recipe.label}
+      title={recipe.recipe.label} 
+      calories={recipe.recipe.calories}
+      image={recipe.recipe.image} 
+      ingredients={recipe.recipe.ingredients} />
+
+    ))}
 
     </div>
-  );
+  )
 }
+
+
 
 export default App;
